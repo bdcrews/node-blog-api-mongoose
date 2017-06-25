9b0649b04d87ad8f147d44bfcc6f0e1b119ba506
Brandon Crews <bdc1978@gmail.com>
2017-06-24 21:29:28 -0700

    add blog api functionality

---
+++ models.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+// this is our schema to represent a blog
+const blogSchema = mongoose.Schema({
+  title: {type: String, required: true},
+  content: {type: String, required: true},
+  author: {
+    firstName: String,
+    lastName: String
+  }
+});
+
+// *virtuals* (http://mongoosejs.com/docs/guide.html#virtuals)
+// allow us to define properties on our object that manipulate
+// properties that are stored in the database. 
+blogSchema.virtual('authorString').get(function() {
+  return `${this.author.firstName} ${this.author.lastName}`.trim()});
+
+// this is an *instance method* which will be available on all instances
+// of the model. This method will be used to return an object that only
+// exposes *some* of the fields we want from the underlying data
+blogSchema.methods.apiRepr = function() {
+
+  return {
+    id: this._id,
+    title: this.title,
+    content: this.content,
+    author: this.authorString
+  };
+}
+
+// note that all instance methods and virtual properties on our
+// schema must be defined *before* we make the call to `.model`.
+const Blog = mongoose.model('Blog', blogSchema);
+
+module.exports = {Blog};
